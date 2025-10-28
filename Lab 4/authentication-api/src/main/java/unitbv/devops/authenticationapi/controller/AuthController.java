5386afd4eb6efb9b8dab2c7b2291eeb28cd90087
AndreiGS <andreibv96@yahoo.com>
2025-10-28 13:47:03 +0200

    Add user service interface

--- Lab 4/authentication-api/src/main/java/unitbv/devops/authenticationapi/controller/AuthController.java
+++ Lab 4/authentication-api/src/main/java/unitbv/devops/authenticationapi/controller/AuthController.java
@@ -4,7 +4,6 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import unitbv.devops.authenticationapi.dto.*;
 import unitbv.devops.authenticationapi.dto.auth.LoginRequest;
 import unitbv.devops.authenticationapi.dto.auth.LoginResponse;
 import unitbv.devops.authenticationapi.dto.auth.RegisterRequest;
--- Lab 4/authentication-api/src/main/java/unitbv/devops/authenticationapi/dto/auth/LoginResponse.java
+++ Lab 4/authentication-api/src/main/java/unitbv/devops/authenticationapi/dto/auth/LoginResponse.java
@@ -1,7 +1,5 @@
 package unitbv.devops.authenticationapi.dto.auth;
 
-import unitbv.devops.authenticationapi.dto.auth.UserResponse;
-
 public record LoginResponse(
         boolean authenticated,
         UserResponse user
